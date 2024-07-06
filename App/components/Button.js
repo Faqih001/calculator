82c9b9def3ec1f80d0758da196c04cf2b615e2a1
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-06 22:05:17 +0300

    Refactor button component styles

--- App/components/Button.js
+++ App/components/Button.js
@@ -48,6 +48,7 @@ export default ({ onPress, text, size, theme }) => {
   // Styles for the button component text styles
   const textStyles = [styles.text];
 
+  // If the size is double, add the double button style
   if (size === "double") {
     buttonStyles.push(styles.buttonDouble);
   }
