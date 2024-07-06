261a6070bff2eee427748b95f37f71fa01ac8cb6
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-06 22:03:31 +0300

    Refactor Row component in Row.js

--- App/components/Row.js
+++ App/components/Row.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 
+// Row component 
 export default ({ children }) => (
   <View style={{ flexDirection: "row" }}>{children}</View>
 );
